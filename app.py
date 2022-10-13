66bd657df514679438a6fbb441743c377ee6c8f7
olganiko95 <olchikbogdanova@yandex.ru>
2022-10-13 20:08:00 +0200

    1

--- app.py
+++ app.py
@@ -4,38 +4,31 @@ from flask import Flask, request
 from flask_restx import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
 from marshmallow import Schema, fields
+from config import app
+from models import Movie, Director, Genre
+from schemas import MovieSchema, GenreSchema, DirectorSchema
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+api = Api(app)
+movie_ns = api.namespace('movies')
+director_ns = api.namespace('director')
+genre_ns = api.namespace('genre')
 
+@movie_ns.route('/')
+class MoviesViews(Resource):
+    def get(self):
+        director_id = request.args.get('director_id')
+        genre_id = request.args.get('genre_id')
+        query = Movie.query
+        if director_id:
+            query = query.filter(Movie.director_id == director_id)
+        if genre_id:
+            query = query.filter(Movie.genre_id == genre_id)
+        return MovieSchema(many=True).dump(Movie.query.all()), 200
 
-class Movie(db.Model):
-    __tablename__ = 'movie'
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(255))
-    description = db.Column(db.String(255))
-    trailer = db.Column(db.String(255))
-    year = db.Column(db.Integer)
-    rating = db.Column(db.Float)
-    genre_id = db.Column(db.Integer, db.ForeignKey("genre.id"))
-    genre = db.relationship("Genre")
-    director_id = db.Column(db.Integer, db.ForeignKey("director.id"))
-    director = db.relationship("Director")
-
-class Director(db.Model):
-    __tablename__ = 'director'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255))
-
-
-class Genre(db.Model):
-    __tablename__ = 'genre'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255))
-
-
+@movie_ns.route('/<int:id>')
+class MovieViews(Resource):
+    def get(self, id):
+        return MovieSchema().dump(Movie.query.get(id)), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
