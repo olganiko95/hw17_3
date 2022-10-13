8e06e6c948403940270de79ddf52acd46496a318
olganiko95 <olchikbogdanova@yandex.ru>
2022-10-13 20:09:12 +0200

    2

---
+++ schemas.py
@@ -0,0 +1,20 @@
+from marshmallow import Schema, fields
+
+class DirectorSchema(Schema):
+    id = fields.Integer(dump_only=True)
+    name = fields.String()
+
+class GenreSchema(Schema):
+    id = fields.Integer(dump_only=True)
+    name = fields.String()
+
+class MovieSchema(Schema):
+    id = fields.Integer(dump_only=True)
+    title = fields.String()
+    description = fields.String()
+    trailer = fields.String()
+    year = fields.Integer()
+    rating = fields.Float()
+    genre = fields.Nested(GenreSchema)
+    director = fields.Nested(DirectorSchema)
+
